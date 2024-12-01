48e6da78e874e23d038b92d1e65be8023967488f
deadsnxw <maks.roskoshenko@gmail.com>
2024-12-01 12:50:34 +0200

    added entry point with text parsing and analysis functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String textInput = "Hello, world! Hello, Aboba? Hello, John.";
+        Text text = new Text(textInput);
+
+        System.out.println("Original Text: " + text);
+
+        TextAnalyzer analyzer = new TextAnalyzer();
+        analyzer.findMostFrequentWord(text);
     }
 }
