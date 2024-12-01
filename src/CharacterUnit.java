8c8e04b9e5cb4ef6cc4a8413e5ee010de3e1b33c
deadsnxw <maks.roskoshenko@gmail.com>
2024-12-01 12:24:38 +0200

    created CharacterUnit class

---
+++ src/CharacterUnit.java
@@ -0,0 +1,16 @@
+public class CharacterUnit {
+    private final char value;
+
+    public CharacterUnit(char value) {
+        this.value = value;
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
